c733249fd7e04234323f1e5ae12d04dbf8bf1133
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2023-10-02 16:53:13 -0700

    cache the flags based on the compiler to speed things up, scripts for setting up and updating

---
+++ update.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Exit the script if any command fails
+set -e
+
+# Initialize compiler variable
+compiler=""
+
+# Function to display script usage
+usage() {
+    echo "Usage: $0 -c <COMPILER>"
+    echo "  -c COMPILER   Specify the compiler name (e.g., gcc or clang)"
+    exit 1
+}
+
+# Parse command-line options using getopt
+while getopts ":c:" opt; do
+  case $opt in
+    c)
+      compiler="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      usage
+      ;;
+  esac
+done
+
+# Check if the compiler argument is provided
+if [ -z "$compiler" ]; then
+  echo "Error: Compiler argument (-c) is required."
+  usage
+fi
+
+# Call the specified scripts with any additional arguments
+./clone.sh
+./change-compiler.sh -c "$compiler"
+./build.sh
+./install.sh
