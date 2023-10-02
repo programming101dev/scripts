242b71e4661f759d1ab94b83dbca469a4cc2798c
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2023-10-01 21:01:23 -0700

    added clone script

---
+++ p101-clone.sh
@@ -0,0 +1,42 @@
+#!/bin/env bash
+
+# Define the target directory where you want to clone repositories
+target_directory=".."
+
+# Array of GitHub repositories to clone
+repositories=(
+    "https://github.com/programming101dev/lib_error.git"
+    "https://github.com/programming101dev/lib_env.git"
+    "https://github.com/programming101dev/lib_c.git"
+    "https://github.com/programming101dev/lib_posix.git"
+    "https://github.com/programming101dev/lib_posix_xsi.git"
+    "https://github.com/programming101dev/lib_posix_optional.git"
+    "https://github.com/programming101dev/lib_unix.git"
+    "https://github.com/programming101dev/c-examples.git"
+)
+
+# Loop through the array and clone repositories
+for repo_url in "${repositories[@]}"; do
+    # Extract the repository name from the URL
+    repo_name=$(basename "$repo_url" .git)
+
+    # Construct the full path to the target directory
+    full_target_directory="$target_directory/$repo_name"
+
+    # Check if the directory already exists
+    if [ -d "$full_target_directory" ]; then
+        echo "Repository '$repo_name' already exists. Skipping."
+    else
+        # Clone the repository
+        git clone "$repo_url" "$full_target_directory"
+
+        # Check if the clone was successful
+        if [ $? -eq 0 ]; then
+            echo "Cloned repository '$repo_name' successfully."
+        else
+            echo "Failed to clone repository '$repo_name'."
+        fi
+    fi
+done
+
+echo "All repositories have been cloned."
