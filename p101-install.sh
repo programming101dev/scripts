b2cb03842996ca5d019f1082ee06a1aa0ad08f4f
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2023-10-01 20:56:34 -0700

    Initial commit

---
+++ p101-install.sh
@@ -0,0 +1,36 @@
+#!/bin/env bash
+
+# List of directories
+directories=(
+    "lib_error"
+    "lib_env"
+    "lib_c"
+    "lib_posix"
+    "lib_posix_xsi"
+    "lib_posix_optional"
+    "lib_unix"
+)
+
+# Loop through the directories
+for dir in "${directories[@]}"; do
+    # Change to the directory
+    cd "$dir" || continue
+
+    # Check if the 'build' directory exists
+    if [ ! -d "build" ]; then
+        # If it doesn't exist, create it and run cmake configure
+        mkdir build
+        cmake -S . -B build
+    fi
+
+    # Run cmake build with clean first
+    cmake --build build --clean-first
+
+    # Run cmake install with sudo
+    sudo cmake --install build
+
+    # Optional: Add additional commands or actions here
+
+    # Return to the original directory
+    cd - || exit
+done
