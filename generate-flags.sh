de446accc73e0a60c2457113c1ddaec8a8e14e1a
programming101dev <113087924+programming101dev@users.noreply.github.com>
2024-02-16 23:02:00 -0800

    better flag support detection

--- generate-flags.sh
+++ generate-flags.sh
@@ -27,36 +27,44 @@ detect_architecture()
 is_flag_supported()
 {
     local compiler="$1"
-    local flag="$2"
-    local supported_flags_ref="$3"
-
+    local tmp_src="$2"
+    local flag="$3"
+    local supported_flags_ref="$4"
     local extra_flags=""
 
     # If the compiler is Clang, set the extra flags
     if [[ "$compiler" == clang* ]]; then
-        extra_flags="-Winvalid-command-line-argument -Wunused-command-line-argument"
+        extra_flags="-Wno-invalid-command-line-argument -Wno-unused-command-line-argument"
     fi
 
-    # Check if the flag is supported by the compiler, including extra flags if set
-    if ("$compiler" "$flag" $extra_flags -Werror -E - < /dev/null &> /dev/null); then
-        echo "Flag '$flag' is supported by $compiler."
-        eval "$supported_flags_ref+=('$flag')"
+    # Attempt to compile and link the temporary source file with the specified flag
+    if $compiler $flag $extra_flags -Werror -o /tmp/test_output "$tmp_src" &> /dev/null; then
+        if [[ -f /tmp/test_output ]]; then
+            echo "Flag '$flag' is supported by $compiler."
+            eval "$supported_flags_ref+=('$flag')"
+        else
+            echo "Flag '$flag' is not supported by the linker. Compilation succeeded but the output is missing."
+        fi
     else
         echo "Flag '$flag' is not supported by $compiler."
     fi
+
+    # Clean up the test output to prevent accumulation of temporary files
+    rm -f /tmp/test_output
 }
 
 # Function to process compiler flags
 process_compiler_flags()
 {
     local compiler="$1"
-    local category="$2"
-    shift 2
+    local tmp_src="$2"
+    local category="$3"
+    shift 3
     local flags=("$@")
     local supported_flags=()
 
     for flag in "${flags[@]}"; do
-        is_flag_supported "$compiler" "$flag" supported_flags
+        is_flag_supported "$compiler" "$tmp_src" "$flag" supported_flags
     done
 
     # Concatenate the flags
@@ -70,20 +78,21 @@ process_compiler_flags()
 process_sanitizer_category()
 {
     local compiler="$1"
-    local category_name="$2"
-    local flags_array_name="$3"
+    local tmp_src="$2"
+    local category_name="$3"
+    local flags_array_name="$4"
     local supported_flags=()
 
     # Convert the string into an actual array
     eval "local flags_array=(\"\${${flags_array_name}[@]}\")"
 
     # Check the first flag
-    is_flag_supported "$compiler" "${flags_array[0]}" supported_flags
+    is_flag_supported "$compiler" "$tmp_src" "${flags_array[0]}" supported_flags
 
     # Then, check the rest of the flags
     for i in "${!flags_array[@]}"; do
         if [[ $i -ne 0 ]]; then  # Skip the first element
-            is_flag_supported "$compiler" "${flags_array[$i]}" supported_flags
+            is_flag_supported "$compiler" "$tmp_src" "${flags_array[$i]}" supported_flags
         fi
     done
 
@@ -99,8 +108,9 @@ process_sanitizer_category()
 process_flags()
 {
     local compiler="$1"
-    local darwin_architecture="$2"
-    local language="$3"
+    local tmp_src="$2"
+    local darwin_architecture="$3"
+    local language="$4"
 
     # https://gcc.gnu.org/onlinedocs/gcc-13.2.0/gcc/Static-Analyzer-Options.html
     local analyzer_flags=(
@@ -234,6 +244,15 @@ process_flags()
       #-m[no-]crc
       #-mgeneral-regs-only
       ###-Winterference-size
+
+      "-fvtable-verify=preinit"
+      "-fvtv-debug"
+      "-fstrict-vtable-pointers"
+      "-fwhole-program-vtables"
+      "-fforce-emit-vtables"
+      "-fno-assume-sane-operator-new"
+      "-fassume-nothrow-exception-dtor"
+      "-fsanitize=shadow-call-stack"
     )
 
     # https://gcc.gnu.org/onlinedocs/gcc-13.2.0/gcc/Warning-Options.html
@@ -453,13 +472,47 @@ process_flags()
       "-Wcast-qual-unrelated"
       "-Wno-thread-safety-analysis"
       "-Wno-thread-safety-negative"
+      "-Wbad-function-cast"
+      "-Wdeclaration-after-statement"
+      "-Wenum-int-mismatch"
+      "-Wimplicit"
+      "-Wjump-misses-init"
+      "-Wmissing-parameter-type"
+      "-Wmissing-prototypes"
+      "-Wnested-externs"
+      "-Wold-style-declaration"
+      "-Wold-style-definition"
+      "-Wpointer-sign"
+      "-Wstrict-prototypes"
+      "-Wc++-compat"
+      "-Wabsolute-value"
+      "-Wduplicate-decl-specifier"
+      "-Wimplicit-function-declaration"
+      "-Wimplicit-int"
+      "-Wincompatible-pointer-types"
+      "-Wint-conversion"
+      "-Woverride-init"
+      "-Wpointer-to-int-cast"
+      "-Wambiguous-member-template"
+      "-Wbind-to-temporary-copy"
+      "-Wabstract-final-class"
+      "-Wabstract-vbase-init"
+      "-Wambiguous-delete"
+      "-Wambiguous-reversed-operator"
+      "-Wanonymous-pack-parens"
+      "-Wauto-disable-vptr-sanitizer"
+      "-Wauto-storage-class"
+      "-Wauto-var-id"
+      "-Wbind-to-temporary-copy"
+      "-Wbinding-in-condition"
+      "-Wcall-to-pure-virtual-from-ctor-dtor"
     )
 
     # https://gcc.gnu.org/onlinedocs/gcc-13.2.0/gcc/Instrumentation-Options.html
     local address_sanitizer_flags=(
         "-fsanitize=address"
         "-fsanitize-address-use-after-scope"
-        "-fsanitize=leakc d "
+        "-fsanitize=leak"
     )
 
     local cfi_sanitizer_flags=(
@@ -520,60 +573,60 @@ process_flags()
         "-fsanitize=vla-bound"
     )
 
-    if [[ $language == "c" ]]; then
-        warning_flags+=("-Wbad-function-cast")
-        warning_flags+=("-Wdeclaration-after-statement")
-        warning_flags+=("-Wenum-int-mismatch")
-        warning_flags+=("-Wimplicit")
-        warning_flags+=("-Wjump-misses-init")
-        warning_flags+=("-Wmissing-parameter-type")
-        warning_flags+=("-Wmissing-prototypes")
-        warning_flags+=("-Wnested-externs")
-        warning_flags+=("-Wold-style-declaration")
-        warning_flags+=("-Wold-style-definition")
-        warning_flags+=("-Wpointer-sign")
-        warning_flags+=("-Wstrict-prototypes")
-        warning_flags+=("-Wc++-compat")
-        warning_flags+=("-Wabsolute-value")
-        warning_flags+=("-Wduplicate-decl-specifier")
-        warning_flags+=("-Wimplicit-function-declaration")
-        warning_flags+=("-Wimplicit-int")
-        warning_flags+=("-Wincompatible-pointer-types")
-        warning_flags+=("-Wint-conversion")
-        warning_flags+=("-Woverride-init")
-        warning_flags+=("-Wpointer-to-int-cast")
-    else
-        warning_flags+=("-Wambiguous-member-template")
-        warning_flags+=("-Wbind-to-temporary-copy")
-        warning_flags+=("-Wabstract-final-class")
-        warning_flags+=("-Wabstract-vbase-init")
-        warning_flags+=("-Wambiguous-delete")
-        warning_flags+=("-Wambiguous-reversed-operator")
-        warning_flags+=("-Wanonymous-pack-parens")
-        warning_flags+=("-Wauto-disable-vptr-sanitizer")
-        warning_flags+=("-Wauto-storage-class")
-        warning_flags+=("-Wauto-var-id")
-        warning_flags+=("-Wbind-to-temporary-copy")
-        warning_flags+=("-Wbinding-in-condition")
-        warning_flags+=("-Wcall-to-pure-virtual-from-ctor-dtor")
+#    if [[ $language == "c" ]]; then
+#        warning_flags+=("-Wbad-function-cast")
+#        warning_flags+=("-Wdeclaration-after-statement")
+#        warning_flags+=("-Wenum-int-mismatch")
+#        warning_flags+=("-Wimplicit")
+#        warning_flags+=("-Wjump-misses-init")
+#        warning_flags+=("-Wmissing-parameter-type")
+#        warning_flags+=("-Wmissing-prototypes")
+#        warning_flags+=("-Wnested-externs")
+#        warning_flags+=("-Wold-style-declaration")
+#        warning_flags+=("-Wold-style-definition")
+#        warning_flags+=("-Wpointer-sign")
+#        warning_flags+=("-Wstrict-prototypes")
+#        warning_flags+=("-Wc++-compat")
+#        warning_flags+=("-Wabsolute-value")
+#        warning_flags+=("-Wduplicate-decl-specifier")
+#        warning_flags+=("-Wimplicit-function-declaration")
+#        warning_flags+=("-Wimplicit-int")
+#        warning_flags+=("-Wincompatible-pointer-types")
+#        warning_flags+=("-Wint-conversion")
+#        warning_flags+=("-Woverride-init")
+#        warning_flags+=("-Wpointer-to-int-cast")
+#    else
+#        warning_flags+=("-Wambiguous-member-template")
+#        warning_flags+=("-Wbind-to-temporary-copy")
+#        warning_flags+=("-Wabstract-final-class")
+#        warning_flags+=("-Wabstract-vbase-init")
+#        warning_flags+=("-Wambiguous-delete")
+#        warning_flags+=("-Wambiguous-reversed-operator")
+#        warning_flags+=("-Wanonymous-pack-parens")
+#        warning_flags+=("-Wauto-disable-vptr-sanitizer")
+#        warning_flags+=("-Wauto-storage-class")
+#        warning_flags+=("-Wauto-var-id")
+#        warning_flags+=("-Wbind-to-temporary-copy")
+#        warning_flags+=("-Wbinding-in-condition")
+#        warning_flags+=("-Wcall-to-pure-virtual-from-ctor-dtor")
 
         # C++ options
         #instrumentation_flags+=("-fsanitize=vptr")
 
         # VTable
         #instrumentation_flags+=("-fvtable-verify=preinit")
-        instrumentation_flags+=("-fvtv-debug")
-        instrumentation_flags+=("-fstrict-vtable-pointers")
-        instrumentation_flags+=("-fwhole-program-vtables")
-        instrumentation_flags+=("-fforce-emit-vtables")
+#        instrumentation_flags+=("-fvtv-debug")
+#        instrumentation_flags+=("-fstrict-vtable-pointers")
+#        instrumentation_flags+=("-fwhole-program-vtables")
+#        instrumentation_flags+=("-fforce-emit-vtables")
 
         # linker
         #instrumentation_flags+=("-f[no]split-lto-unit")
 
         # memory options
-        instrumentation_flags+=("-fno-assume-sane-operator-new")
-        instrumentation_flags+=("-fassume-nothrow-exception-dtor")
-    fi
+#        instrumentation_flags+=("-fno-assume-sane-operator-new")
+#        instrumentation_flags+=("-fassume-nothrow-exception-dtor")
+ #   fi
 
     if [[ "$darwin_architecture" == "arm64" ]]; then
         instrumentation_flags+=("-fcf-protection=null")
@@ -583,7 +636,7 @@ process_flags()
         fi
     else
         instrumentation_flags+=("-fcf-protection=full")
-        instrumentation_flags+=("-fsanitize=shadow-call-stack")
+#        instrumentation_flags+=("-fsanitize=shadow-call-stack")
     fi
 
     echo "Checking: $compiler"
@@ -594,24 +647,33 @@ process_flags()
     rm -f "$flag_dir"/*
 
     # Process each flag category
-    process_compiler_flags "$compiler" "analyzer" "${analyzer_flags[@]}"
-    process_compiler_flags "$compiler" "debug" "${debug_flags[@]}"
-    process_compiler_flags "$compiler" "instrumentation" "${instrumentation_flags[@]}"
-    process_compiler_flags "$compiler" "optimization" "${optimization_flags[@]}"
-    process_compiler_flags "$compiler" "optimize" "${optimize_flags[@]}"
-    process_compiler_flags "$compiler" "warning" "${warning_flags[@]}"
-    process_sanitizer_category "$compiler" "address_sanitizer" "address_sanitizer_flags"
-    process_sanitizer_category "$compiler" "cfi_sanitizer" "cfi_sanitizer_flags"
-    process_sanitizer_category "$compiler" "dataflow_sanitizer" "dataflow_sanitizer_flags"
-    process_sanitizer_category "$compiler" "hwaddress_sanitizer" "hwaddress_sanitizer_flags"
-    process_sanitizer_category "$compiler" "memory_sanitizer" "memory_sanitizer_flags"
-    process_sanitizer_category "$compiler" "pointer_overflow_sanitizer" "pointer_overflow_sanitizer_flags"
-    process_sanitizer_category "$compiler" "safe_stack_flags" "safe_stack_flags"
-    process_sanitizer_category "$compiler" "thread_sanitizer_flags" "thread_sanitizer_flags"
-    process_sanitizer_category "$compiler" "undefined_sanitizer" "undefined_sanitizer_flags"
+    process_compiler_flags "$compiler" "$tmp_src" "analyzer" "${analyzer_flags[@]}"
+    process_compiler_flags "$compiler" "$tmp_src" "debug" "${debug_flags[@]}"
+    process_compiler_flags "$compiler" "$tmp_src" "instrumentation" "${instrumentation_flags[@]}"
+    process_compiler_flags "$compiler" "$tmp_src" "optimization" "${optimization_flags[@]}"
+    process_compiler_flags "$compiler" "$tmp_src" "optimize" "${optimize_flags[@]}"
+    process_compiler_flags "$compiler" "$tmp_src" "warning" "${warning_flags[@]}"
+    process_sanitizer_category "$compiler" "$tmp_src" "address_sanitizer" "address_sanitizer_flags"
+    process_sanitizer_category "$compiler" "$tmp_src" "cfi_sanitizer" "cfi_sanitizer_flags"
+    process_sanitizer_category "$compiler" "$tmp_src" "dataflow_sanitizer" "dataflow_sanitizer_flags"
+    process_sanitizer_category "$compiler" "$tmp_src" "hwaddress_sanitizer" "hwaddress_sanitizer_flags"
+    process_sanitizer_category "$compiler" "$tmp_src" "memory_sanitizer" "memory_sanitizer_flags"
+    process_sanitizer_category "$compiler" "$tmp_src" "pointer_overflow_sanitizer" "pointer_overflow_sanitizer_flags"
+    process_sanitizer_category "$compiler" "$tmp_src" "safe_stack_flags" "safe_stack_flags"
+    process_sanitizer_category "$compiler" "$tmp_src" "thread_sanitizer_flags" "thread_sanitizer_flags"
+    process_sanitizer_category "$compiler" "$tmp_src" "undefined_sanitizer" "undefined_sanitizer_flags"
 }
 
 darwin_architecture=$(detect_architecture)
+
+# Create a temporary source file at the start
+tmp_c_src=$(mktemp "/tmp/test_src_XXXXXX.c")
+echo "int main(void) { return 0; }" > "$tmp_c_src"
+
+tmp_cxx_src=$(mktemp "/tmp/test_src_XXXXXX.cpp")
+echo "int main(void) { return 0; }" > "$tmp_cxx_src"
+
+trap "rm -f '$tmp_c_src' '$tmp_cxx_src'" EXIT
 
 # Read the list of supported compilers and process each
 supported_c_compilers=()
@@ -626,10 +688,10 @@ done < supported_cxx_compilers.txt
 
 # Process C compilers
 for compiler in "${supported_c_compilers[@]}"; do
-    process_flags "$compiler" "$darwin_architecture" "c"
+    process_flags "$compiler" "$tmp_c_src" "$darwin_architecture" "c"
 done
 
 # Process C++ compilers
 for compiler in "${supported_cxx_compilers[@]}"; do
-    process_flags "$compiler" "$darwin_architecture" "cxx"
+    process_flags "$compiler" "$tmp_cxx_src" "$darwin_architecture" "cxx"
 done
