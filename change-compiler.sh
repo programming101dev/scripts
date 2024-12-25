fd00fac4b1c4bea6890b439d4b6f20ba51ea4129
programming101dev <ds@programming101.dev>
2024-12-24 22:29:03 -0800

    remove default sanitizers

--- change-compiler.sh
+++ change-compiler.sh
@@ -8,7 +8,7 @@ cxx_compiler=""
 clang_format_name="clang-format"
 clang_tidy_name="clang-tidy"
 cppcheck_name="cppcheck"
-sanitizers="address,undefined,leak"
+sanitizers="address,leak,pointer_overflow,undefined"
 
 usage()
 {
